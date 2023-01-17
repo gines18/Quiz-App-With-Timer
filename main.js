e08232dea5acfd3743e1cc6d3c977cc1440b0e4e
Mario <ruczkowski.m@gmail.com>
2023-01-17 23:40:35 +0000

    Add variables with questions

---
+++ main.js
@@ -0,0 +1,44 @@
+let questions = [
+    {
+      question: "What is the correct syntax for declaring a variable in JavaScript?",
+      options: [
+        "var x;",
+        "int x;",
+        "let x;",
+        "const x;"
+      ],
+      answer: "var x;"
+    },
+    {
+      question: "What is the purpose of the `document.getElementById()` method?",
+      options: [
+        "To return the first element with a specified ID",
+        "To change the text content of an element",
+        "To create a new element in the document",
+        "To return the value of a specified attribute"
+      ],
+      answer: "To return the first element with a specified ID"
+    },
+    {
+      question: "Which of the following is not a JavaScript data type?",
+      options: [
+        "String",
+        "Number",
+        "Array",
+        "File"
+      ],
+      answer: "File"
+    },
+    {
+      question: "What does the following code snippet do?",
+      question_code: "let arr = [1, 2, 3, 4]; let newArr = arr.map(function(x) { return x * 2; });",
+      options: [
+        "It declares an array and multiplies each element by 2",
+        "It declares an array and sorts the elements in descending order",
+        "It declares an array and adds all the elements",
+        "It declares an array and removes all the elements"
+      ],
+      answer: "It declares an array and multiplies each element by 2"
+    }
+  ];
+  
